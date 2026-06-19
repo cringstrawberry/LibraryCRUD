f98d7353f05388a4b7e9bcecb92e5d6be6c07ee9
cringstrawberry <kimminji056050@gmail.com>
2026-06-19 10:22:01 +0900

    test

---
+++ src/main/java/com/server/librarycrud/board/controller/testController.java
@@ -0,0 +1,10 @@
+package com.server.librarycrud.board.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
